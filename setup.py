4d90d818a1a4d0229dd9a0d68444d3079d838d9e
Koen van Eijk <vaneijk.koen@gmail.com>
2025-09-17 09:04:24 +0200

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="zenllm",
-    version="0.3.1",
+    version="0.3.2",
     author="Koen van Eijk",
     description="A zen, simple, and unified API to prompt LLMs from Anthropic, Google, OpenAI, and more, using only the requests library.",
     long_description=long_description,
